e16d58f756bc87c60b51b2d73c6c521586b53a9e
pamarsh907 <peter_aaron_marsh@hotmail.com>
2023-06-22 13:14:22 -0800

    created header section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,26 @@
+import Header from './components/header'
 import './globals.css'
 import { Inter } from 'next/font/google'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'IMDB Clone',
+  title: 'IMDb Clone',
   description: 'This is the IMBb Clone website',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className }>
+        {/*Header*/}
+        <Header />
+        {/*Navbar*/}
+
+        {/*Searchbox*/}
+
+        {children}</body>
     </html>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Home</h1>
+    <main>
+      <h1 className='text-red-400'>Page</h1>
     </main>
   )
 }
