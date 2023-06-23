c44fe93214bc3d36ccc19b7d983cec291ae3ad32
pamarsh907 <peter_aaron_marsh@hotmail.com>
2023-06-22 17:27:31 -0800

    fetch data from tmdb api

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,30 @@
-import Image from 'next/image'
+import Results from "./components/results";
 
-export default function Home() {
+const API_KEY = process.env.API_KEY;
+
+
+
+export default async function Home({ searchParams }) {
+
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+    genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+  }?api_key=${API_KEY}&language=en-US&page=1`,
+  { next: { revalidate: 10000 } }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const data = await res.json();
+  const results = data.results;
+
   return (
-    <main>
-      <h1 className='text-red-400'>Page</h1>
-    </main>
+
+  <div>
+    <Results results={results}/>
+  </div>
   )
 }
