83b839e39a1b083963ff1be331097348ac907296
pamarsh907 <peter_aaron_marsh@hotmail.com>
2023-06-27 19:14:47 -0800

    added loading effect

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Loading() {
+  return (
+    <div className="flex justify-content">
+        <img classNames="h-96" rc="spinner.svg" alt="loading..." />
+    </div>
+  )
+}
