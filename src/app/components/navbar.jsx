33d6be726695dd0605740f3cd6ae758ef973385c
pamarsh907 <peter_aaron_marsh@hotmail.com>
2023-06-28 16:40:06 -0800

    added name to navbar component

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import NavbarItem from './navbar-item'
 
-export default function () {
+export default function Navbar() {
   return (
     <div className='flex justify-center dark:bg-gray-600 bg-amber-100 lg:text-lg p-4'>
         <NavbarItem title="Trending" param="fetchTrending" />
